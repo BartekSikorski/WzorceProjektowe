8e91e2e594722263727aa5e3127c7493c5c15f75
Bartek Sikorski <b.sikorski@iteam.pl>
2021-04-05 12:21:43 +0200

    EmailDecorator

--- WzorceProjektowe/Strukturalne/Docorator/I/Client.cs
+++ WzorceProjektowe/Strukturalne/Docorator/I/Client.cs
@@ -5,15 +5,21 @@ namespace WzorceProjektowe.Strukturalne.Docorator.I
     public class Client
     {
         private static bool sendSMS;
+        private static bool sendEmail;
 
         public static void Execute()
         {
             var message = Console.ReadLine();
             var notification = (INotification)new Notification();
-            sendSMS = true;
+            sendSMS = sendEmail = true;
             if (sendSMS)
             {
                 notification = new SmsNotification(notification);
+            }
+            if (sendEmail)
+            {
+                notification = new EmailNotification(notification);
+
             }
 
             notification.Send(message);
---
+++ WzorceProjektowe/Strukturalne/Docorator/I/EmailNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WzorceProjektowe.Strukturalne.Docorator.I
+{
+    public class EmailNotification : NotificationBaseDecorator
+    {
+        public EmailNotification(INotification notification) : base(notification)
+        {
+        }
+
+        public override void SendExtra(string message)
+        {
+            Console.WriteLine($"Wysłano E-mail: {message}.");
+        }
+    }
+}
